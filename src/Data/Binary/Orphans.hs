7df2c7021d664212705a1e8778b873b67520238d
Daniel Mendler <mail@daniel-mendler.de>
2017-01-20 23:21:55 +0100

    add Binary instance for Hashed (#24)
    * add Binary instance for Hashed

--- src/Data/Binary/Orphans.hs
+++ src/Data/Binary/Orphans.hs
@@ -41,7 +41,7 @@ import qualified Data.CaseInsensitive as CI
 import qualified Data.Fixed as Fixed
 import qualified Data.HashMap.Lazy as HM
 import qualified Data.HashSet as HS
-import           Data.Hashable (Hashable)
+import qualified Data.Hashable as Hashable
 import           Data.List (unfoldr, foldl')
 import qualified Data.List.NonEmpty as NE
 import qualified Data.Monoid as Monoid
@@ -82,13 +82,19 @@ instance Binary A.Value where
   put A.Null       = put (5 :: Int)
 
 
-instance  (Hashable k, Eq k, Binary k, Binary v) => Binary (HM.HashMap k v) where
+instance  (Hashable.Hashable k, Eq k, Binary k, Binary v) => Binary (HM.HashMap k v) where
   get = fmap HM.fromList get
   put = put . HM.toList
 
-instance (Hashable v, Eq v, Binary v) => Binary (HS.HashSet v) where
+instance (Hashable.Hashable v, Eq v, Binary v) => Binary (HS.HashSet v) where
   get = fmap HS.fromList get
   put = put . HS.toList
+
+#if MIN_VERSION_hashable(1,2,5)
+instance (Hashable.Hashable a, Binary a) => Binary (Hashable.Hashed a) where
+  get = fmap Hashable.hashed get
+  put = put . Hashable.unhashed
+#endif
 
 #if !(MIN_VERSION_scientific(0,3,4))
 instance Binary S.Scientific where
@@ -243,7 +249,7 @@ instance Binary a => Binary (NE.NonEmpty a) where
 instance Binary m => Binary (Semigroup.WrappedMonoid m) where
   get = fmap Semigroup.WrapMonoid get
   put = put . Semigroup.unwrapMonoid
- 
+
 -- | /Since: binary-orphans-0.1.5.0/
 instance (Binary a, Binary b) => Binary (Semigroup.Arg a b) where
   get                     = liftM2 Semigroup.Arg get get
